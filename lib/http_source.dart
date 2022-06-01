a11ef088c9814f1a4ba12af20fb0e29ce95dde1f
Alexis Rios Cabrera <alexis.riosc2701@gmail.com>
2022-05-31 21:59:06 -0500

    source

---
+++ lib/http_source.dart
@@ -0,0 +1,5 @@
+library http_source;
+
+export './ahttp_source.dart';
+export './models/models.dart';
+export './utils/pair.dart';
---
+++ lib/models/models.dart
@@ -0,0 +1,6 @@
+export './chapter.dart';
+export './filter.dart';
+export './filter_list.dart';
+export './manga.dart';
+export './manga_details.dart';
+export './mangas_page.dart';
